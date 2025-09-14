52f19c9f6fee31eb2b328df18f71508b1dc5d01a
akshaydileep005 <akshaydileep005@gmail.com>
2025-09-14 13:08:38 +0530

    updated akshay

--- edudonate/src/main/java/com/edudonate/edudonate/controller/ExchangeController.java
+++ edudonate/src/main/java/com/edudonate/edudonate/controller/ExchangeController.java
@@ -27,7 +27,7 @@ public class ExchangeController {
     // Show form to create new listing
     @GetMapping("/new")
     public String showNewExchangeForm(Model model) {
-        model.addAttribute("exchanges", new Exchange());
+        model.addAttribute("exchange", new Exchange());
         return "new-exchange";
     }
 
